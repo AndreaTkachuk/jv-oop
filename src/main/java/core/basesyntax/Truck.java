fd761ce1ab14784a701e71468a645caa3b9dc48d
AndreaTkachuk <kovklad@gmail.com>
2025-09-14 00:06:04 +0200

    changed is working to started working

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Truck extends Machine {
     @Override 
     public void doWork() {
-        System.out.println("Truck is working!!!");
+        System.out.println("Truck started working");
     }
     
     @Override 
     public void stopWork() {
-        System.out.println("Truck is stoped!!!");
+        System.out.println("Truck stopped working");
     }
 }
